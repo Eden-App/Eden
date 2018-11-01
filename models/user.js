b57d0a5f210c821acce09a0cdd179cab016018a6
Indira Dayo <indiradayo@live.com>
2018-11-01 18:22:33 +0700

    initial files

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose'),
+      Schema = mongoose.Schema
+
+
+const userSchema = new Schema({
+    name: String,
+    email: String,
+    password: String
+})
+
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
