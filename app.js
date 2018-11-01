36e996c0ab3bfa5706dda76fe394302afeb545eb
Indira Dayo <indiradayo@live.com>
2018-11-01 23:49:38 +0700

    tambah semua api manga

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express'),
+      cors = require('cors'),
+      mongoose = require('mongoose'),
+      db = mongoose.connection,
+
+      port = process.env.PORT || 3000,
+      app = express()
+
+
+// mongoose.connect('mongodb://localhost:27017/eden-app')
+// db
+//     .on('error', console.error.bind(console, 'database connection error:'))
+//     .once('open', function() {
+//         console.log('database connected')
+//     });
+
+
+const mangaRouter = require('./routes/mangaeden')
+
+app
+    .use(express.urlencoded({ extended:false }))
+    .use(express.json())
+    .use(cors())
+
+    .use('/mangas', mangaRouter)
+
+
+    .listen(port, () => {
+        console.log(`I'm listening to port ${port}`)   
+    })
